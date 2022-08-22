90a9337da4ecb79a36313d8bc3bf333c2ef58662
TrinhDuc997 <trinhxuanduc997@gmail.com>
2022-08-22 12:23:23 +0700

    remove pageExtensions in next config and export default index.ts in layout

--- pages/Components/Layout/index.ts
+++ pages/Components/Layout/index.ts
@@ -1,3 +1,6 @@
+import MainLayout from './main'
+
 export {default as MainLayout} from './main'
 export {default as EmptyLayout} from './empty'
 export {default as AdminLayout} from './admin'
+export default MainLayout
